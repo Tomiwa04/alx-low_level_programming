94046b9b4b365d4969b7c6b4e1f8e34a94d6aa75
Precious <adeyeyeprecious04@gmail.comcleargit config --global user.email adeyeyeprecious04@gmail.com>
2022-09-09 13:16:02 -0700

    Print 1 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int u;
+
+	for (u = 0; u < 10; u++)
+	{
+	printf("%d", u)
+	}
+	printf("\n")
+	return (0);
+}
