ab680ed8b6587e07759d014a5521bc75c73ccc7c
Precious <adeyeyeprecious04@gmail.comcleargit config --global user.email adeyeyeprecious04@gmail.com>
2022-09-09 09:41:53 -0700

    Positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Sucess)
  */
@@ -21,7 +21,7 @@ int main(void)
 
 	else if (n < 0)
 	{
-		printf("%d is negative\n", n)
+		printf("%d is negative\n", n);
 	}
 
 	else
