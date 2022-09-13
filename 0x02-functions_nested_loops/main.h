92d0bc49c38ef4f4252ce81498f364d0f6312949
Precious <adeyeyeprecious04@gmail.comcleargit config --global user.email adeyeyeprecious04@gmail.com>
2022-09-13 10:18:17 -0700

    Print putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,34 @@
+#ifndef MAIN_H_INCLUDED
+
+#define MAIN_H_INCLUDED
+
+
+
+int _putchar(cahr c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_baurer(void);
+
+void times_table(void);
+
+int add(Int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+
+#endif
