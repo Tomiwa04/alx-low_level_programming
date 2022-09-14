2aee5587d5893b480ff5f7a35c583a5980f54bd3
Precious <adeyeyeprecious04@gmail.comcleargit config --global user.email adeyeyeprecious04@gmail.com>
2022-09-14 13:41:22 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,11 @@
  */
 
 int print_last_digit(int n)
+
 {
 	int last_digit;
 
-	if (n < o)
+	if (n < 0)
 	{
 		last_digit = (-1 * (n % 10));
 		_putchar (last_digit + '0');
@@ -24,4 +25,5 @@ int print_last_digit(int n)
 		_putchar (last_digit + '0');
 		return (last_digit);
 	}
+
 }
