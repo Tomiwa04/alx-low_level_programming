135544229ead5cbadbc2a78dba2e0de2b7185001
Precious <adeyeyeprecious04@gmail.comcleargit config --global user.email adeyeyeprecious04@gmail.com>
2022-09-14 11:57:55 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,13 +7,13 @@
  */
 
 void jack_bauer(void)
+
 {
 	int a;
 	int b;
 
 	for (a = 0; a <= 23; a++)
 	{
-
 		for (b = 0; b <= 59; b++)
 		{
 			_putchar (a / 10 + '0');
@@ -26,4 +26,5 @@ void jack_bauer(void)
 		}
 
 	}
+
 }
