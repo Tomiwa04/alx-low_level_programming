29f6567bb77f0e22fa63dbdfcc290a51e627a2c2
Precious <adeyeyeprecious04@gmail.comcleargit config --global user.email adeyeyeprecious04@gmail.com>
2022-09-16 04:43:23 -0700

    Print 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - prints number
- */
+ * print_numbers - prints 0-9
+ *
+ * Return: nothing
+ **/
 
 void print_numbers(void)
 {
-	int i;
+	char i;
 
-	for (i = 48; i < 58; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		_putchar(i);
 	}
