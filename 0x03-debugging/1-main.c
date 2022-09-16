201f28f19298c8240d444d9e8d197f90b627f057
Precious <adeyeyeprecious04@gmail.comcleargit config --global user.email adeyeyeprecious04@gmail.com>
2022-09-16 07:33:52 -0700

    Main c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - causes an infinite loop
- * Return: 0
+ * main - causes and infinite loop
+ * Return: 0 for success
  */
 
 int main(void)
@@ -13,12 +13,10 @@ int main(void)
 
 	i = 0;
 
-	/**
-	 * while (i < 10)
-	 * {
-	 * _putchar(i);
-	 * }
-	 */
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
