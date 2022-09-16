3fa7193af626038cc6781bc04275d343422b0305
Precious <adeyeyeprecious04@gmail.comcleargit config --global user.email adeyeyeprecious04@gmail.com>
2022-09-15 20:37:18 -0700

    Header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,25 @@
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for prototype
+ * void prototype (void)
+ * int prototype (int)
+ */
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int);
+void print_times_tables(int);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -10,3 +31,5 @@ void print_diagonal(int n);
 void print_square(int n);
 void print_triangle(int size);
 void print_number(int n);
+
+#endif /* MAIN_H */
