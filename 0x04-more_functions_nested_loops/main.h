5ca2316310720e382faeb73a18bae33cace863d8
Precious <adeyeyeprecious04@gmail.comcleargit config --global user.email adeyeyeprecious04@gmail.com>
2022-09-15 20:41:32 -0700

    Header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -22,14 +22,15 @@ void print_to_98(int);
 void print_times_tables(int);
 int _isupper(int c);
 int _isdigit(int c);
-int mul(int a, int b);
+int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int n);
-void print_triangle(int size);
-void print_number(int n);
+void more_numbers( void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
+void print_number(int);
 
 #endif /* MAIN_H */
+
