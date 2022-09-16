70acbc0920e6e1fc3016213bd748c8aed3cb025f
Precious <adeyeyeprecious04@gmail.comcleargit config --global user.email adeyeyeprecious04@gmail.com>
2022-09-16 07:15:52 -0700

    More number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,20 +9,14 @@ void more_numbers(void)
 {
 	int i, j;
 
-	i = 0;
-
-	while (i < 10)
+	for (j = 0; j <= 9; j++)
 	{
-		for (j = 0; j < 15; j++)
+		for (i = 0; i <= 14; i++)
 		{
-			if (j >= 10)
-			{
-				_putchar((j / 10) + 48);
-			}
-			_putchar((j % 10) + 48;
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
-
-		i++;
 	}
 }
