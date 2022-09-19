68bc2e336a237bb902ac46cae3d530b70ea64731
Precious <adeyeyeprecious04@gmail.comcleargit config --global user.email adeyeyeprecious04@gmail.com>
2022-09-19 16:41:53 -0700

    Length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that retuens the length of a string
+ * @s: char input
+ * Return: length of the input string
+ */
+int _strlen(char *s)
+{
+	int i = 1, sum = 0
+	char p1 = s[0];
+
+	while (p1 != '\0')
+	{
+		sum++;
+		pl = s[i++]
+	}
+	return (sum);
+}
