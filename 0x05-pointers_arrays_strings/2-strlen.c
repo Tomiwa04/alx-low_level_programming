14cbcffc7f38ad19f9d8ba276a4c48e1da7e5590
Precious <adeyeyeprecious04@gmail.comcleargit config --global user.email adeyeyeprecious04@gmail.com>
2022-09-19 20:33:30 -0700

    Strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,13 +7,13 @@
  */
 int _strlen(char *s)
 {
-	int i = 1, sum = 0
+	int i = 1, sum = 0;
 	char pl = s[0];
 
 	while (pl != '\0')
 	{
 		sum++;
-		pl = s[i++]
+		pl = s[i++];
 	}
 	return (sum);
 }
