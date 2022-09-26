34f6627e0229f95cac53201ef427f7cac5f7e5b0
Precious <adeyeyeprecious04@gmail.comcleargit config --global user.email adeyeyeprecious04@gmail.com>
2022-09-26 16:40:56 -0700

    Fills memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,20 @@
 #include "main.h"
+
 /**
  * _memset - fills memory with a constant byte
  * @s: pointer block of memory to fill
- * @b: value to set
- * @n: bytes of the memory
- * Return: dest
- */
+ * @b: constant byte used to replace
+ * @n: number of bytes to replace
+ * Description: fills memory with byte b for n byte
+ * Return: pointer to memery
+ **/
 
 char *_memset(char *s, char b, unsigned int n)
-
 {
-	unsigned int i;
-
-	/*Declaring FOR*/
-	for (i = 0; i < n; i++)
+	for (; n > 0; n--)
 	{
-		*(s + i) = b; /*add 1 position s*/
-
-	} /*END FOR*/
+		s[n - 1] = b;
+	}
 
 	return (s);
 }
