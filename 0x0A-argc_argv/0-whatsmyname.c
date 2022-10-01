7469d677b2fe99b8a737c6107d27cbb1d3932c93
Precious <adeyeyeprecious04@gmail.comcleargit config --global user.email adeyeyeprecious04@gmail.com>
2022-10-01 03:03:48 -0700

    Print name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Entry point
- *
- * @argc: Counts the number of parameters that go into main
- * @argv: Pointer of array of pointers containing strings entering main
+ * main - prints name of file
+ * @argc: number of arguments
+ * @argv: arguments entered
  * Return: Always 0(Success)
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
+	argc = argc;
+
 	return (0);
 }
