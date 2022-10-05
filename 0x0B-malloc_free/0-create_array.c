7162fd865b6bae512d0329689f408eebcd876daa
Precious <adeyeyeprecious04@gmail.comcleargit config --global user.email adeyeyeprecious04@gmail.com>
2022-10-04 19:53:20 -0700

    Create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, and initializes it wil
+ * a specific char
+ * @size: size of array
+ * @c: character to intialize with
+ *
+ * Return: Pointer
+ */
+char *create_array(unsigned int size, cgar c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size <= 0)
+		return (0);
+
+	s = malloc(sizeof(char) * size);
+
+	if (s == 0)
+		return (0);
+
+	for (i = 0; i < size; i++)
+		*(s + i) = c;
+
+	*(s + i) = '\0';
+
+	return (s);
+}
