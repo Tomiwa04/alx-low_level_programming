d7cf9c12dddc668cde7be49d2533ec6285864e8e
Precious <adeyeyeprecious04@gmail.comcleargit config --global user.email adeyeyeprecious04@gmail.com>
2022-10-10 19:21:00 -0700

    Dog.h

--- 0x0E-structures_typedof/dog.h
+++ 0x0E-structures_typedof/dog.h
@@ -1,5 +1,6 @@
-#ifndef STRUCTS
-#define STRUCTS
+#ifndef FILE_DOG
+#define FILE_DOG
+
 /**
  * struct dog - description for a pet
  * @name: pet name
@@ -8,16 +9,16 @@
  *
  * Description: Longer description
  */
-typedef struct dog
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-#endif
-#ifndef _FUNCTIONS_H
-#define _FUNCTIONS_H
-int _putchar(char c);
+};
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
